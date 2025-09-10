81c9f9af2cc43eeb0bdd76af1d24f6874992de04
jonne <jonne.vuorela@proton.me>
2025-09-10 15:18:07 +0300

    request handlerit

---
+++ cmd/web/handlers.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) home(writer http.ResponseWriter, request *http.Request) {
+	data := app.newTemplateData(request)
+
+	app.render(writer, http.StatusOK, "home.tmpl", data)
+}
+
+func ping(writer http.ResponseWriter, request *http.Request) {
+	writer.Write([]byte("OK"))
+}
