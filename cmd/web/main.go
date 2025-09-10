e77c486d4472b361e684777279f55437a950936f
jonne <jonne.vuorela@proton.me>
2025-09-10 15:08:29 +0300

    http server pohjustusta, perus kohdat app structille

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,34 @@
 package main
 
-import ()
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
 
 type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
 }
 
 func main() {
-	_ = &application{}
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	infoLog := log.New(os.Stdout, "\033[42;30mINFO\033[0m\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "\033[41;30mERROR\033[0m\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
+
+	srv := &http.Server{
+		Addr:         *addr,
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
 }
