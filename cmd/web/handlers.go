b3d71d0a60e720d19928c4b00141bd3a33508cd4
Jonne Vuorela <jonne.vuorela@proton.me>
2025-10-02 17:11:20 +0300

    websocket pakettien vastaanoton hiontaa

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,8 +1,23 @@
 package main
 
 import (
+	"encoding/binary"
+	"math"
 	"net/http"
 )
+
+type Vec3 struct {
+	x float64
+	y float64
+	z float64
+}
+
+type Vec4 struct {
+	x float64
+	y float64
+	z float64
+	w float64
+}
 
 func (app *application) home(writer http.ResponseWriter, request *http.Request) {
 	data := app.newTemplateData(request)
@@ -27,30 +42,47 @@ func (app *application) websocket(w http.ResponseWriter, r *http.Request) {
 	app.infoLog.Printf("WebSocket connected: %v", conn.LocalAddr().String())
 
 	for {
-		var msg map[string]interface{}
-		err := conn.ReadJSON(&msg)
+		_, data, err := conn.ReadMessage()
 		if err != nil {
 			app.infoLog.Printf("WebSocket read error: %v", err)
 			break
 		}
-		playerID, _ := msg["player_id"].(float64)
-		action, _ := msg["action"].(map[string]interface{})
-		app.infoLog.Printf("Received from player %v: %v", int(playerID), action)
 
-		response := map[string]interface{}{
-			"entities": []map[string]interface{}{
-				{
-					"id":       int(playerID),
-					"position": map[string]float64{"x": 0, "y": 3, "z": 0},
-					"velocity": map[string]float64{"x": 0, "y": 0, "z": 0},
-				},
-			},
+		floats := make([]float64, len(data)/8)
+		for i := range floats {
+			bits := binary.LittleEndian.Uint64(data[i*8 : (i+1)*8])
+			floats[i] = math.Float64frombits(bits)
 		}
-		err = conn.WriteJSON(response)
-		if err != nil {
-			app.infoLog.Printf("WebSocket write error: %v", err)
-			break
-		}
+		// floats[0] playerId
+		// floats[1:4] position (x,y,z)
+		// floats[4:8] rotation (x,y,z,w)
+		// floats[8:11] velocity (x,y,z)
+		app.infoLog.Printf("Received data from player %.0f:\npos(%.2f, %.2f, %.2f)\nrot(%.2f, %.2f, %2f, %2f)\nvel(%.2f, %.2f, %.2f)\n",
+			floats[0],
+			floats[1], floats[2], floats[3],
+			floats[4], floats[5], floats[6], floats[7],
+			floats[8], floats[9], floats[10])
+
+		//	playerId := floats[0]
+		//	pos := Vec3{floats[1], floats[2], floats[3]}
+		//	rot := Vec4{floats[4], floats[5], floats[6]}
+		//	vel := Vec3{floats[8], floats[9], floats[10]}
+
+		//	response := map[string]any{
+		//		"entities": []map[string]any{
+		//			{
+		//				"id":       int(playerId),
+		//				"position": map[string]float64{"x": 0, "y": 3, "z": 0},
+		//				"rotation": map[string]float64{"x": 0, "y": 3, "z": 0, "w": 1},
+		//				"velocity": map[string]float64{"x": 0, "y": 0, "z": 0},
+		//			},
+		//		},
+		//	}
+		//	err = conn.WriteJSON(response)
+		//	if err != nil {
+		//		app.infoLog.Printf("WebSocket write error: %v", err)
+		//		break
+		//	}
 	}
 	app.infoLog.Println("WebSocket disconnected")
 }
