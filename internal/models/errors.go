8c8cc61b2f92a447c469d1ac92a11553ccab7165
Jonne Vuorela <jonne.vuorela@proton.me>
2025-10-03 17:09:34 +0300

    data model erroreita

---
+++ internal/models/errors.go
@@ -0,0 +1,11 @@
+package models
+
+import (
+	"errors"
+)
+
+var (
+	ErrNoRecord           = errors.New("models: no matching record found")
+	ErrInvalidCredentials = errors.New("models: invalid credentials")
+	ErrDuplicateUsername  = errors.New("models: duplicate username")
+)
