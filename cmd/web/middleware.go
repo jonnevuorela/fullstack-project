6ffe73837e18603bfcdf765748b1104af33245a5
Jonne Vuorela <jonne.vuorela@proton.me>
2025-10-03 17:12:47 +0300

    helpereitä autentikaatiota varten

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -20,6 +20,19 @@ func noSurf(next http.Handler) http.Handler {
 	return csrfHandler
 }
 
+func (app *application) requireAuthentication(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !app.isAuthenticated(r) {
+			http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+			return
+		}
+
+		w.Header().Add("Cache-Control", "no-store")
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func secureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// random nonce
