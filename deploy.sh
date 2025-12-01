5a83f94a43fa6efc28ac43ca280f0e6abe3bb7db
Jonne Vuorela <jonne.vuorela@proton.me>
2025-12-01 18:57:34 +0200

    polkujen parametrisointia

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -euo pipefail
 
-LOGFILE="/home/jonne/fullstack-project/deploy.log"
+LOGFILE="$HOME/fullstack-project/deploy.log"
 exec > >(tee -a "$LOGFILE") 2>&1
 
 echo "===== Deployment started at $(date) ====="
 
-cd /home/jonne/fullstack-project
+cd $HOME/fullstack-project
 
 git pull --rebase
 
-cd /home/jonne/fullstack-project/game
+cd $HOME/fullstack-project/game
 
 echo "[$(date)] Running npm install..."
 npm install
@@ -18,7 +18,7 @@ npm install
 echo "[$(date)] Running npm run build..."
 npm run build
 
-cd /home/jonne/fullstack-project
+cd $HOME/fullstack-project
 
 echo "[$(date)] Running go build..."
 go build ./cmd/web
@@ -31,7 +31,7 @@ echo "===== Deployment completed successfully at $(date) ====="
 
 echo "===== Restarting database container $(date) ====="
 
-cd /home/jonne/fullstack-project/mariadb-docker
+cd $HOME/fullstack-project/mariadb-docker
 docker-compose down
 docker-compose up -d
 
